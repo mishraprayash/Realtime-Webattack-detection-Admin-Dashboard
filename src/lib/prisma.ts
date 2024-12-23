083dd4de75d2c3a5abaa6296524a0264b10c3192
mishraprayash <mishraprayash11@gmail.com>
2024-12-23 20:52:07 +0545

    updated prisma logging

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'], // Enable detailed logging
+  log: ['error'], // Enable detailed logging
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
